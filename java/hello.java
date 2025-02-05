6391062204a9bcd40dfdf92462c0aa03566545ab
ABarsen <kiplagatemmanuel21@gmail.com>
2025-02-05 23:18:33 +0300

    hello java

---
+++ java/hello.java
@@ -0,0 +1,8 @@
+class Hello {
+    public static void main(String[] args) {
+        int age = 20;
+        
+
+        System.out.println("Im " + age + " years old today and maybe tommorrow");
+    }
+}
